be51282286bbc4d78199dba5ccabf88d1445520a
vsantos1711 <vsantos067100@gmail.com>
2022-09-04 19:45:03 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// const imagens = document.querySelectorAll('[src^="./img/imagem"]');
+// imagens.forEach(function(item) {
+//     console.log(item)
+// })
+
+// const linksInternos = document.querySelectorAll('[href^="#"]');
+// linksInternos.forEach(function(item){
+//     console.log(item);
+// })
+
+// const primeiroH2 = document.querySelector('.animais-descricao h2');
+// console.log(primeiroH2.innerHTML);
+
+// const paragrafos = document.querySelectorAll('p');
+// console.log(paragrafos[paragrafos.length - 1].innerText);
