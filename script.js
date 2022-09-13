9b30f7efcbc716794025930f12d871abe6daaae8
vsantos1711 <vsantos067100@gmail.com>
2022-09-12 21:23:03 -0300

    smooth scroll

--- script.js
+++ script.js
@@ -36,3 +36,22 @@ function initAccordion() {
     }
 }
 initAccordion();
+
+function smoothScroll() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    if(linksInternos.length){
+        function scollToSection(event){
+            event.preventDefault();
+            const href = event.currentTarget.getAttribute('href');
+            const section = document.querySelector(href);
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }
+        linksInternos.forEach((link) => {
+            link.addEventListener('click', scollToSection);
+        });
+    }
+}
+smoothScroll();
