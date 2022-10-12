eaea6e163aea4449c5f8b4f5212abaf6ff6683c8
vsantos1711 <vsantos067100@gmail.com>
2022-10-12 16:18:13 -0300

    new script.js

--- script.js
+++ script.js
@@ -77,3 +77,19 @@ function initAnimacaoScroll(){
 }
 initAnimacaoScroll();
 
+function Carro (marca, preco) {
+    this.marca = marca;
+    this.preco = preco;
+}
+
+const honda = new Carro('Honda', 4000);
+
+
+function Carro2 (marca, precoInicial) {
+    const taxa = 1.2;
+    const precoFinal = precoInicial * taxa;
+    this.marca = marca;
+    this.preco = precoFinal
+}
+
+const mazda = new Carro2('Mazda', 5000);
