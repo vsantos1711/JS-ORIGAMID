7921bf6e55f4d0e24b5a41e7a5f1e43de8b4ebf7
vsantos1711 <vsantos067100@gmail.com>
2022-10-29 16:31:59 -0300

    Modal de login

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ import initTabNav from "./modules/initTabNav.js";
 import initAccordion from "./modules/initAccordion.js";
 import smoothScroll from "./modules/smoothScroll.js";
 import initAnimacaoScroll from "./modules/initAnimacaoScroll.js";
+import initModal from "./modules/initModal.js";
+
 
 initTabNav();
 initAccordion();
 smoothScroll();
 initAnimacaoScroll();
+initModal();
