309d02a4d8e9769604bcb2f6841c8d7fb147dd46
vsantos1711 <vsantos067100@gmail.com>
2022-11-04 14:26:33 -0300

    Clickoutside

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,12 +1,20 @@
+import outsideClick from "./outsideclick";
+
 export default function initDropdownMenu(){
     const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
     dropdownMenus.forEach(menu =>{
-        menu.addEventListener('click', handleClick);
+        ['touchstar', 'click'].forEach(userEvent =>{
+            menu.addEventListener(userEvent, handleClick);
+        });
     })
     function handleClick(){
         event.preventDefault();
-        console.log(event)
+        this.classList.add('active')
+        outsideClick(this, ['touchstart', 'click'], () =>{
+            this.classList.remove('active');
+        });
     }
+    
 };
 
