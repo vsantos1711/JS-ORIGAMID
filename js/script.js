496a175fa7eb25f9961036837ffa14a0a801126c
vsantos1711 <vsantos067100@gmail.com>
2022-11-10 14:20:46 -0300

    Anima Números

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown.js';
 import initMenuMobile from './modules/menu-mobile.js';
-
+import initAnimaNumeros from './modules/anima-numeros.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -16,3 +16,4 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
+initAnimaNumeros();
