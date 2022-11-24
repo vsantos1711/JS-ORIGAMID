cdcf9592ec3637bb5365763b2fd1ca7b2cd8f8e3
vsantos1711 <vsantos067100@gmail.com>
2022-11-24 13:43:01 -0300

    Adição do botão e inicio do completar cep

--- cep.js
+++ cep.js
@@ -1,16 +1,39 @@
 const formulario = document.querySelector('#endereco');
+const btn = document.querySelector('#btn');
 const endereco = {};
 
-function handleChange(event) {
+function salvarInformacoesForm(event) {
     endereco[event.target.name] = event.target.value;
-    pesquisaCep(endereco.cep)
 }
-formulario.addEventListener('change', handleChange)
 
-function pesquisaCep(cep){
-    fetch('https://viacep.com.br/ws/'+ cep+ '/json/')
+formulario.addEventListener('change', salvarInformacoesForm);
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const cep = endereco.cep
+    if(endereco.cep != ''){
+        pesquisaPorCep(cep)
+    }
+    // descobrirCep(endereco);    
+})
+// function descobrirCep(endereco){
+    
+// }
+function pesquisaPorCep(cep){
+    fetch('https://viacep.com.br/ws/'+cep+'/json/')
     .then(response => response.json())
     .then((body) => {
-        console.log(body)
+        rua.value = body.logradouro
+        bairro.value = body.bairro
+        cidade.value = body.localidade
+        estado.value = body.uf
+        ibge.value = body.ibge
     })
 }
+
+// function descobrirCep(estado, cidade, rua, bairro){
+//     fetch('httpx://viacep.com.br/ws'+ estado +'/'+ cidade +'/'+ rua + '/json/')
+//     .then(response => response.json())
+//     .then((body) => {
+
+//     })
+// }
